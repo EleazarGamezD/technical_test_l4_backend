053ddf94665b4910f8995e592afb0387b705351a
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-19 16:07:31 -0500

    Refactor TaskController: update routes and methods for task management, remove old controller and tests

--- src/modules/task/controllers/task.controller.ts
+++ src/modules/task/controllers/task.controller.ts
@@ -1,3 +1,4 @@
+import { IPaginate } from '@core/interfaces/paginate.interface';
 import { LuxonDatePipe } from '@core/pipes/luxon-date.pipe';
 import {
   Body,
@@ -5,15 +6,20 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
-  Put,
   Query,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
 import { TaskService } from '../services/task.service';
-import { IPaginate } from '@core/interfaces/paginate.interface';
 
 @ApiTags('tasks')
 @Controller('tasks')
@@ -21,22 +27,32 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiBody({ type: CreateTaskDto })
   create(@Body(new LuxonDatePipe()) createTaskDto: CreateTaskDto) {
     console.log(createTaskDto);
     return this.taskService.create(createTaskDto);
   }
 
   @Get('all')
+  @ApiOperation({ summary: 'Get all tasks with pagination' })
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
   findAll(@Query() pagination: IPaginate) {
     return this.taskService.findAll(pagination);
   }
 
   @Get('get/:id')
+  @ApiOperation({ summary: 'Get a task by ID' })
+  @ApiParam({ name: 'id', type: Number })
   findOne(@Param('id') id: number) {
     return this.taskService.findOne(id);
   }
 
-  @Put('update/:id')
+  @Patch('update/:id')
+  @ApiOperation({ summary: 'Update a task by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: UpdateTaskDto })
   update(
     @Param('id') id: number,
     @Body(new LuxonDatePipe()) updateTaskDto: UpdateTaskDto,
@@ -45,6 +61,8 @@ export class TaskController {
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ summary: 'Delete a task by ID' })
+  @ApiParam({ name: 'id', type: Number })
   remove(@Param('id') id: number) {
     this.taskService.remove(id);
     return 'Task deleted successfully';
